a9ea56e77dc320b5c0b1a79f3405ccde5ca2e460
Devin Mullin <devin.mullin90@gmail.com>
2021-11-11 17:29:29 -0600

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@ class ArticlesController < ApplicationController
 
   def index
     articles = Article.where(is_member_only: false).includes(:user).order(created_at: :desc)
+    
     render json: articles, each_serializer: ArticleListSerializer
   end
 
